82b5cbe6e82ec5c7db48d7406e2c016cbf626137
mamantube <firmanhiday.id007@gmail.com>
2025-05-11 22:06:08 +0700

    update code

--- routes/api.php
+++ routes/api.php
@@ -8,7 +8,6 @@ use Illuminate\Support\Facades\Route;
 Route::post("/register", [UserController::class, "register"]);
 Route::post("/login", [UserController::class, "login"]);
 Route::patch("/users/{id}", [UserController::class, "updateUser"]);
-Route::get("/users/{id}", [UserController::class, "user"]);
 Route::get("products/{id}", [ProductController::class, "product"]);
 Route::get("/products", [ProductController::class, "allProducts"]);
 
@@ -16,6 +15,7 @@ Route::get("/products", [ProductController::class, "allProducts"]);
 Route::middleware(['auth:sanctum'])->group(function () {
     
     Route::get("/users", [UserController::class, "allUser"]);
+    Route::get("/users/{id}", [UserController::class, "user"]);
     Route::delete("/users/{id}", [UserController::class, "deleteUser"]);
 
     Route::prefix('products')->group(function () {
