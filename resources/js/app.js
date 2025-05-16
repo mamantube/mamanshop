d05bf0fecfa7a58a079df84ba8fba00aeec5255a
mamantube <firmanhiday.id007@gmail.com>
2025-05-16 15:50:26 +0700

    import index css

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,6 +5,8 @@ import router from './router';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import "bootstrap-icons/font/bootstrap-icons.css"
+import "../css/index.css"
+
 
 
 
